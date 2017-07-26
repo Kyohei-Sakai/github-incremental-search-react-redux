a5cd595ed286fab80b9a8e500eb805e9b3de90e0
Kyohei Sakai <nico_f00tb@yahoo.co.jp>
2017-07-26 10:27:30 +0900

    Change setting github access token

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
 export default {
-  GITHUB_ACCESS_TOKEN: process.env.GITHUB_ACCESS_TOKEN,
+  // GITHUB_ACCESS_TOKEN: process.env.GITHUB_ACCESS_TOKEN,
+  GITHUB_ACCESS_TOKEN: '********',
   GITHUB_BASE_URL: 'https://api.github.com',
 }
